6a7ce7f443fc8518d1c3836c8672eba19b947f0c
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2024-05-23 07:59:33 +0000

    I added the bio data form but i did'nt complete it

--- app/(front)/onboarding/[id]/page.tsx
+++ app/(front)/onboarding/[id]/page.tsx
@@ -2,8 +2,8 @@ import OnboardingSteps from '@/components/onboarding/OnboardingSteps'
 import React from 'react'
 
 export default function page({params: {id} }: {params: {id: string}}) {
-  return (
-    <div className=''>
+  return ( 
+    <div className=' lg:mt-[50px]'>
     <div className="max-w-6xl mx-auto lg:py-10 min-h-screen">
     <OnboardingSteps/>
     </div>
--- components/onboarding/OnboardingSteps.tsx
+++ components/onboarding/OnboardingSteps.tsx
@@ -1,24 +1,24 @@
 "use client"
+import Link from 'next/link';
 import { useSearchParams } from 'next/navigation'
 import React from 'react'
 
 export default function OnboardingSteps() {
-const params = useSearchParams()
-const page = params.get("page") ?? 5;
-console.log(page)
-
+  const params = useSearchParams()
+  const page = params.get("page") ?? 5;
+  console.log(page)
 
   return (
-    <div className='grid grid-cols-12 mx-auto'>
-      <div className="col-span-full sm:col-span-2 bg-red-600">
-        <h1>Step1</h1>
-        <h1>Step1</h1>
-        <h1>Step1</h1>
-        <h1>Step1</h1>
-        <h1>Step1</h1>
-        <h1>Step1</h1>
+    <div className='grid grid-cols-12 mx-auto shadow-inner
+    overflow-hidden   min-h-screen dark:bg-white bg-slate-200'>
+      <div className="col-span-full  sm:col-span-2 divide-y-2 divide-gray-200">
+        <Link href="#" className='block py-4 px-4 bg-best uppercase  dark:bg-slate-950 text text-slate-100 shadow-inner '>Bio Information</Link>
+        <Link href="#" className='block py-4 px-4 bg-slate-300   dark:bg-slate-950 text text-slate-800 shadow-inner '>Step1</Link>
+        <Link href="#" className='block py-4 px-4 bg-slate-300  dark:bg-slate-950 text text-slate-800 shadow-inner '>Step1</Link>
+        <Link href="#" className='block py-4 px-4 bg-slate-300  dark:bg-slate-950 text text-slate-800 shadow-inner '>Step1</Link>
+        <Link href="#" className='block py-4 px-4 bg-slate-300  dark:bg-slate-950 text text-slate-800 shadow-inner '>Step1</Link>
       </div>
-      <div className="sm:col-span-10 col-span-full bg-green-500">
+      <div className="sm:col-span-10 col-span-full dark:text-black p-4">
         <h1>Form1</h1>
         <h1>Form1</h1>
         <h1>Form1</h1>
