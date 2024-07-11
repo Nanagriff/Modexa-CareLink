bba8bab3302514acce9082290aad066e7f48a18b
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2024-07-11 22:35:12 +0000

    added the opt page

---
+++ app/(front)/verifyotp/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { VerifyOtp } from './VerifyOtp'
+
+export default function page() {
+  return (
+    <div className='mt-[200px] flex justify-center'>
+      <VerifyOtp/>
+    </div>
+  )
+}
